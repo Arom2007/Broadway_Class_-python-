8535edf74a3de7c45e34b4e8801d76679010ec41
Arom2007 <arompradhan@gmail.com>
2024-01-02 12:13:47 +0545

    2nd January Commit

---
+++ day 2/Data_Types.py
@@ -0,0 +1,9 @@
+# There are broadly two types of datatypes in Python
+# 1. Mutable Datatypes
+# 2. Immutable Datatypes
+
+# 1. Mutable Datatypes: All those objects which can be modified after their creation are the mutable datatypes/objects
+# Eg. List, Dictionary, Set
+
+# 2. Immutable Datatypes: All those objects which can't be modified after their creation are the immutable datatypes/objects
+# Eg. String, Tuple, Numbers, Boolean
