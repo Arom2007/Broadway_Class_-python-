58b2f861d989dde4789f361f647117590a647e6a
Arom2007 <arompradhan@gmail.com>
2024-01-05 12:15:15 +0545

    minor addition

--- day 5/Task_5.py
+++ day 5/Task_5.py
@@ -1,6 +1,8 @@
-# WAP to prompt the user for hours and rate per hour using input to compute gross pay. Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours
+# WAP to prompt the user for hours and rate per hour using input to compute gross pay.
+# Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours
 # worked above 40 hours.
-# Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). You should use input to read a string, float() to convert the string to number.
+# Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75).
+# You should use input to read a string, float() to convert the string to number.
 
 hours = float(input("Enter hours: "))
 rate_per_hour = float(input("Enter the rate per hour: "))
