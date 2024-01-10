830736c023d01489780dcb887a93b7c5ca42aca4
Arom2007 <arompradhan@gmail.com>
2024-01-10 11:11:51 +0545

    non

--- Exercises/2_Function_Exercises.py
+++ Exercises/2_Function_Exercises.py
@@ -27,4 +27,26 @@ def reverse(string_name):
 print("Reverse of the given string is " + reverse("Aroma"))
 
 
-# Write a python function to check whether a number falls within a given range.
+# Write a python function to check whether a given number falls within a given range.
+def is_in_range(numb, ran):
+    if numb in ran:
+        return "Number is in range."
+    else:
+        return "Number is not in range."
+
+
+numb = int(input("Enter a number: "))
+in_range = is_in_range(numb, range(1, 100))
+print(in_range)
+
+
+# Write a python function to multiply all the numbers in a list.
+def product(numbers):
+    total = 1
+    for x in numbers:
+        total *= x
+    return total
+
+
+prod = product([1, 2, 3, 4, 5])
+print(f"Product of all numbers in the list is {prod}")
